c8cfcafb114308758e4ccdb277433bf998915896
Sixto-Castro93 <sjcastro@espol.edu.ec>
2016-12-16 02:35:08 -0500

    LRU Policy with Zipfian Generator - First Version

--- src/main/java/com/cache/cache_lhm_bf/Cache_lhm.java
+++ src/main/java/com/cache/cache_lhm_bf/Cache_lhm.java
@@ -8,7 +8,10 @@ package com.cache.cache_lhm_bf;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 
 /**
  *
@@ -38,6 +41,12 @@ public class Cache_lhm {
         String line = null;
         String option = null;
         float hit_rate = 0;
+        HashSet<Long> uniqueKeys = new HashSet<Long>();
+        Random rand = new Random();
+        int numKeysZipfian = 1000000;
+        double ZipfConf = 0.7;
+        ZipfianGenerator zipf = new ZipfianGenerator(numKeysZipfian, ZipfConf);
+
         
         if(replacement_policy == 1){     //opcion 1 = LRU Cache 
             option = "LRU Cache";
@@ -144,15 +153,48 @@ public class Cache_lhm {
 //            }
         }
         
+        if(replacement_policy == 6){     //opcion 1 = LRU Cache 
+            option = "LRU Cache - zipfian";
+            cache = new LRUCache(capacity);
+            //int num_access=30;
+            int num_access=(20*32*capacity)+(capacity*10000);
+            int i=0;
+            while( num_access > 0 ) {
+                num_access--; 
+                i++;
+                total_access = total_access + 1;
+                long rnd = zipf.nextValue();
+                //System.out.println("rand number " +i+ ": "+rnd);
+                uniqueKeys.add(rnd);
+                String file_id = Long.toString(rnd);
+                if (cache.get(file_id) == null){
+                    cache.set(file_id, Cache_lhm.value_default);
+                }
+                else{
+                    total_hits =  total_hits + 1;
+                }
+            }
+            /*System.out.println("unique keys: ");
+            Object[] array = uniqueKeys.toArray();
+            for (int j = 0; j< array.length; j++) {
+                System.out.println(array[j]);
+            }*/
+            System.out.println("Unique keys: " + uniqueKeys.size());
+            
+        }
+        
         System.out.println(option);
         System.out.printf("Number of accessed file:  %d \n", total_access);
         System.out.printf("Number of hits:  %d \n", total_hits);
         hit_rate = (float)total_hits / total_access;
         System.out.printf("Hit rate %.7f \n", hit_rate);
-        
+        long warmUpPeriod = 20*32*capacity;
+        double warm_hit_rate = (float)(total_hits - warmUpPeriod) / (total_access - warmUpPeriod);
+        System.out.printf("Warm hit rate %.7f \n", warm_hit_rate);
+     }   
                 
         
-    }
+    
     
     public static void print_lhm (LRUCache cache){
         for(Map.Entry<String, String> e : cache.entrySet()) {
